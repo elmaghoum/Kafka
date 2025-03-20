2db81275368785bf7915f9c5d3c08f68338fe552
EL MAGHOUM Faycal <elmaghoum.faycal@outlook.fr>
2025-03-20 13:12:08 +0100

    Ex2.2 Kafka fonctionnel

---
+++ src/main/java/MADM/kafka/CustomPartitioner.java
@@ -0,0 +1,21 @@
+package MADM.kafka;
+
+import java.util.Map;
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+public class CustomPartitioner implements Partitioner {
+    private static final int PARTITION_COUNT = 2;
+
+    @Override
+    public void configure(Map<String, ?> configs) {}
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        Integer keyInt = Integer.parseInt(key.toString());
+        return keyInt % PARTITION_COUNT;
+    }
+
+    @Override
+    public void close() {}
+}
---
+++ src/main/java/MADM/kafka/ProdMain.java
@@ -0,0 +1,23 @@
+package MADM.kafka;
+
+public class ProdMain {
+    public static void main(String[] args) {
+        System.out.println("Hello prod!");
+
+        int message = 1000;
+        int numberOfProducers = 2;
+
+
+        int t= message/numberOfProducers;
+
+        for (int i = 0; i < numberOfProducers; i++) {
+            
+            int producerId = i;
+            Thread producerThread = new Thread(() -> {
+                Producteur p = new Producteur(t, t*producerId);
+                p.runProducer(producerId);
+            });
+            producerThread.start();
+        }
+    }
+}
